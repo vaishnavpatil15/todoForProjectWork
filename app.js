0baf35bb83eb90109b3e4e4fd03829ddc84fb9fb
vaishnavpatil15 <vaishnav.b.patil12@gmail.com>
2025-03-29 13:52:28 +0530

    added origin to cors

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require("cors")
 const db = require("./config/db.config")
 db();
 
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173"
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
